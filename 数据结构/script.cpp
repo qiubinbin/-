e46f143309cad164fa0bec6a6f3db8d1eac2bc71
qiubinbin <qiubin780061109@163.com>
2018-07-05 01:29:40 +0800

    添加项目文件。

---
+++ 数据结构/script.cpp
@@ -0,0 +1,39 @@
+struct List
+{
+	int length;
+	Elem * elem;
+	int listsize;
+};
+void MERGE(List A, List B, List & C) {
+	int a, b = A.length, B.length;
+	Elem* la, lb = A.elem, B.elem;//起始地址
+	Elem* fa, fb = A.elem + la, B.elem + lb;//结束的地址
+	Elem lc = (Elem*)malloc((a + b) * sizeof(Elem));
+	if (!lc.elem) exit (OVERFLOW);
+	while (la<=fa && lb<=fb)
+	{
+		if (*la <= *lb) {
+			*lc = *la;
+			la++;
+			lc++;
+		}
+		else
+		{
+			*lc = *lb;
+			lb++;
+			lc++;
+		}
+	}
+	while (la<=fa)//插入剩余的A中的元素
+	{
+		*lc = *la;
+		la++;
+		lc++;
+	}
+	while (lb<=fb)//插入剩余的B中的元素
+	{
+		*lc = *lb;
+		lb++;
+		lc++;
+	}
+}
---
+++ 数据结构/test1.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int i = 0;
+	for (i; i < 5; i++)
+	{
+		cout << i << endl;
+	}
+	return 0;
+}
